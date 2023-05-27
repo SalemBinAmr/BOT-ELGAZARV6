c60cccab3d322ca44d68cab1bc5e0cd99f280863
SalemBinAmr <95317603+SalemBinAmr@users.noreply.github.com>
2023-05-27 07:16:42 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,16 +22,16 @@ global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.ownernomer = "967739359648" //ur number
+global.premium = ['967739359648'] //ur premium number
+global.botname = '❤️𝚂𝙱-𝙱𝙾𝚃❤️' //ur bot name
+global.linkz = "https://youtube.com/@SBMODS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.botscript = 'https://youtube.com/@SBMODS' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣بوت سالم بن عمرو يروحي⍣" //ur sticker watermark packname
+global.author = "⍣967739359648⍣" //ur sticker watermark author
+global.wm = "❤️𝚂𝙱-𝙱𝙾𝚃❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
